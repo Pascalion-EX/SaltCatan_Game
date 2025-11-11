4a554b4238b0de1ee490026c638a384c6a7c1a20
KFam <kerolos.a.fam@gmail.com>
2025-11-11 11:03:32 +0200

    Final version without trading3

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = ({ setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/users/login`, formData);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/users/login`, formData)
       localStorage.setItem("token", res.data.token);
       console.log(res.data);
       setUser(res.data);
