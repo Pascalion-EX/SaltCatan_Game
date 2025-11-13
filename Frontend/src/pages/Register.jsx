424a8af7fb929717889a08a3f9767485ae3fbe0b
KFam <kerolos.a.fam@gmail.com>
2025-11-14 01:28:51 +0200

    Final ensha2 ellah

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -18,7 +18,11 @@ const Register = ({ setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/users/login`, formData);
+      const res = await axios.post(
+  `${import.meta.env.VITE_API_URL}/api/users/register`,
+  formData
+);
+
       localStorage.setItem("token", res.data.token);
       console.log(res.data);
       setUser(res.data);
