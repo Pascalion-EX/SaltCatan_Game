cd9fa22965a3d74e63e7da0431cf0811ccc6a943
KFam <kerolos.a.fam@gmail.com>
2025-11-21 22:22:51 +0200

    In-game was a bad game

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -32,65 +32,12 @@ const Register = ({ setUser }) => {
     }
   };
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md border border-gray-200">
-        <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
-          Register
-        </h2>
-        {error && <p className="text-red-500 mb-4 text-sm">{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm font-medium mb-1">
-              Username
-            </label>
-            <input
-              className="w-full p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-200 outline-none focus:border-blue-400"
-              type="username"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              placeholder="Enter your username"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm font-medium mb-1">
-              Email
-            </label>
-            <input
-              className="w-full p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-200 outline-none focus:border-blue-400"
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Enter your email"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-600 text-sm font-medium mb-1">
-              Password
-            </label>
-            <input
-              className="w-full p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-200 outline-none focus:border-blue-400"
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          <button className="w-full bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 font-medium cursor-pointer">
-            Register
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
-
+return (
+<div className="min-h-screen flex items-center justify-center bg-gray-100">
+<div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md border border-gray-200 text-center">
+<h1 className="text-4xl font-bold text-gray-800">HELLO FADY</h1>
+</div>
+</div>
+);
+}
 export default Register;
